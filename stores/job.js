519e86832df727c647632d054776996e2da315b1
hialdev <al.developer12@gmail.com>
2024-02-28 11:54:59 +0700

    Menormalkan Store Jobs ke yang terbaru

--- stores/job.js
+++ stores/job.js
@@ -53,10 +53,8 @@ export const useJobStore = defineStore('job',{
             this.selectedJob = job;
         },
         async getJobs(page_size = this.filter.page_size, location = this.filter.location, contract = this.filter.contract, title = this.filter.search){
-            const access = localStorage.getItem('access_token') ?? 'rbkmzydqknor0t5q236n01j38';
-            let url = '/joborder/read';
-            var headers = new Headers();
-                headers.append("token",access ?? 'rbkmzydqknor0t5q236n01j38');
+            const access = localStorage.getItem('access_token');
+            let url = '/joborder/readpublic';
 
             if(access){
                 url = '/joborder/read';
